3b22fd1413eb8c9a3b0156f9eda55df0e935267f
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-09-30 16:16:07 +0400

    Add simple wrapper around user/{ids}/answers

--- src/Network/StackExchange.hs
+++ src/Network/StackExchange.hs
@@ -1,9 +1,22 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE ViewPatterns #-}
 module Network.StackExchange where
 
+import Control.Applicative ((<$>), empty)
+import Control.Exception (Exception, throwIO)
+import Data.List (intercalate)
+import Data.Typeable (Typeable)
+
+import qualified Data.Aeson as A
+import qualified Data.Aeson.Types as A
+import qualified Data.Attoparsec.Lazy as AP
 import qualified Data.ByteString.Lazy as B
+import           Network.HTTP.Conduit
 
 
 data Object =
@@ -11,28 +24,65 @@ data Object =
   | AccountMerge
   | Answer
   | Badge
+  | BadgeCount
   | Comment
-  | Edit
-  | Event
   | Error
+  | Event
   | Filter
-  | Inbox
+  | InboxItem
   | Info
-  | NetworkUser
+  | MigrationInfo
+  | Notice
   | Notification
   | Post
-  | Question
   | Priviledge
+  | Question
+  | QuestionTimeline
+  | RelatedSite
   | Reputation
   | ReputationHistory
   | Revision
   | Site
+  | Styling
+  | SuggestedEdit
   | Tag
   | TagScore
   | TagSynonym
-  | Timeline
   | TagTop
+  | TagWiki
   | User
+  | UserNetwork
+  | UserShallow
+  | UserTimeline
+  | WritePermission
 
 
-data Response (a ∷ Object) = Response B.ByteString
+data SE (a ∷ Object) = SE A.Value deriving Show
+
+
+newtype SEException = SEException String
+  deriving (Show, Typeable)
+
+
+instance Exception SEException
+
+
+users'ids'answers ∷ [Int] → IO [SE Answer]
+users'ids'answers (intercalate ";" . map show → ids) =
+  AP.parse A.json <$> simpleHttp
+    (stackexchange ++ "users/" ++ ids ++ "/answers?order=desc&sort=activity&site=stackoverflow") >>= \case
+    AP.Done _ s → case A.parse p s of
+      A.Success v → return $ map SE v
+      _ → throwSE ".users/{ids}/answers: Incorrect JSON, cannot as a list of answers"
+    _ → throwSE ".users/{ids}/answers: Malformed JSON, cannot parse"
+ where
+  p (A.Object o) = o A..: "items"
+  p _ = empty
+
+
+stackexchange ∷ String
+stackexchange = "https://api.stackexchange.com/2.1/"
+
+
+throwSE ∷ String → IO a
+throwSE = throwIO . SEException . ("libstackexchange" ++)
