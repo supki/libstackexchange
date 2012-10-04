04077cb25498a4fa3641e868d0d68a4cfb3f0a99
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-10-04 21:46:22 +0400

    Reorganize modules a bit. Use Network.StackExchange for the ease of importing.

--- src/Network/StackExchange.hs
+++ src/Network/StackExchange.hs
@@ -1,73 +1,9 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE UnicodeSyntax #-}
-{-# LANGUAGE ViewPatterns #-}
+-- | Convenience module for the ease of importing
 module Network.StackExchange
-  ( runStackExchange
-  , users'ids'answers
-  , filter'create
+  ( module S
   ) where
 
-import Control.Applicative (empty)
-import Control.Exception (throwIO)
-import Control.Monad (liftM)
-
-import           Control.Monad.State (MonadState, get, put)
-import           Control.Monad.Trans (MonadIO, liftIO)
-import           Data.Monoid.Lens ((<>=))
-import qualified Data.Aeson as A
-import qualified Data.Aeson.Types as A
-import qualified Data.Attoparsec.Lazy as AP
-import           Data.ByteString.Lazy (ByteString)
-import           Data.Text.Lazy (Text, intercalate)
-import           Data.Text.Lazy.Builder (toLazyText)
-import           Data.Text.Lazy.Builder.Int (decimal)
-import           Network.HTTP.Conduit (simpleHttp)
-
-import Control.Monad.StackExchange (StackExchangeT, runStackExchange)
-import Network.StackExchange.URI
-import Network.StackExchange.Types
-
-
-users'ids'answers ∷ MonadIO m ⇒ [Int] → StackExchangeT m [SE Answer]
-users'ids'answers (intercalate ";" . map (toLazyText . decimal) → ids) = localState $ do
-  uriPath <>= ["users", ids, "answers"]
-  uriQuery <>= [("order","desc"),("sort","activity"),("site","stackoverflow")]
-  AP.parse A.json `liftM` request >>= \case
-    AP.Done _ s → case A.parse p s of
-      A.Success v → return $ map SE v
-      _ → throwSE ".users/{ids}/answers: Incorrect JSON, cannot parse as a list of answers"
-    _ → throwSE ".users/{ids}/answers: Malformed JSON, cannot parse"
- where
-  p (A.Object o) = o A..: "items"
-  p _ = empty
-
-
-filter'create ∷ MonadIO m ⇒ [Text] → [Text] → Text → StackExchangeT m (SE Filter)
-filter'create (intercalate ";" → include) (intercalate ";" → exclude) base = localState $ do
-  uriPath <>= ["filter", "create"]
-  uriQuery <>= [("include",include),("exclude",exclude),("base",base)]
-  AP.parse A.json `liftM` request >>= \case
-    AP.Done _ s → return $ SE s
-    _ → throwSE ".filter/create: Malformed JSON, cannot parse"
-
-
-localState ∷ MonadState s m ⇒ m a → m a
-localState m = get >>= \s → m >>= \v → put s >> return v
-{-# INLINE localState #-}
-
-
-request ∷ MonadIO m ⇒ StackExchangeT m ByteString
-request = get >>= io . simpleHttp . render
-{-# INLINE request #-}
-
-
-throwSE ∷ MonadIO m ⇒ String → m a
-throwSE = io . throwIO . SEException . ("libstackexchange" ++)
-{-# INLINE throwSE #-}
-
-
-io ∷ MonadIO m ⇒ IO a → m a
-io = liftIO
-{-# INLINE io #-}
+import Control.Monad.StackExchange as S
+import Network.StackExchange.API as S
+import Network.StackExchange.URI as S
+import Network.StackExchange.Types as S
