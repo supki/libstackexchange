34f73c955c893bc0ea3f59e818e39d07d3e53d0e
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-10-20 21:17:36 +0400

    Embed filter and site into query.

--- src/Network/StackExchange/Request.hs
+++ src/Network/StackExchange/Request.hs
@@ -8,14 +8,14 @@ module Network.StackExchange.Request
   ( -- * Types
     Request(..), Auth(..)
     -- * Construct request
-  , path, parse, query, site
+  , path, parse, query, site, filter
     -- * Schedule request
   , askSE
   ) where
 
-import Data.Foldable (foldMap)
 import Data.Monoid (Monoid(..), (<>))
 import GHC.TypeLits
+import Prelude hiding (filter)
 
 import           Data.ByteString.Lazy (ByteString)
 import           Data.Default (Default(..))
@@ -32,8 +32,6 @@ data Auth = Yes | No
 data Request (a ∷ Auth) (i ∷ Nat) r = Request
   { _host ∷ Text
   , _path ∷ Text
-  , _filter ∷ Text
-  , _site ∷ Text
   , _query ∷ Map Text Text
   , _parse ∷ Maybe (ByteString → Maybe r)
   }
@@ -43,17 +41,13 @@ instance Monoid (Request a i r) where
   mempty = Request
     { _host = mempty
     , _path = mempty
-    , _site = mempty
-    , _filter = mempty
     , _query = mempty
     , _parse = Nothing
     }
   l `mappend` r = Request
     { _host = _host $ if T.null $ _host r then l else r
     , _path = _path $ if T.null $ _path r then l else r
-    , _site = _site $ if T.null $ _site r then l else r
-    , _filter = _filter $ if T.null $ _filter r then l else r
-    , _query = _query l <> _query r
+    , _query = _query r <> _query l
     , _parse = _parse $ case _parse r of Just _ → r; Nothing → l
     }
 
@@ -76,14 +70,18 @@ query q = mempty {_query = M.fromList q}
 
 
 site ∷ Text → Request a i r
-site s = mempty {_site = s}
+site s = mempty {_query = M.singleton "site" s}
+
+
+filter ∷ Text → Request a i r
+filter f = mempty {_query = M.singleton "filter" f}
 
 
 -- | To use Request in http-conduit we need to convert it to a string
 render ∷ Request a i r → String
-render r = T.unpack . mconcat $ [_host r, "/", _path r, "?site=", _site r, argie . M.toList $ _query r]
+render r = T.unpack . mconcat $ [_host r, "/", _path r, "?", argie $ _query r]
  where
-  argie = foldMap (\(a, b) → mconcat ["&", a, "=", b])
+  argie = T.intercalate "&" . M.foldrWithKey (\k v m → T.concat [k, "=", v] : m) mempty
 
 
 askSE ∷ Request a i r → IO (Either ByteString r)
