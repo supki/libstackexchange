cb5566d4232fdccd0d96d02a81a60ccec860f63a
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-10-23 18:52:17 +0400

    Cosmetics.

--- examples/badges-watcher.hs
+++ examples/badges-watcher.hs
@@ -4,22 +4,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE UnicodeSyntax #-}
 
+import Data.Foldable (foldMap)
 import Data.Monoid
 
 import Control.Lens
 
-import Network.StackExchange hiding (filter)
+import Network.StackExchange
 
 
 main ∷ IO ()
-main =
-  askSE (badgesOnUsers [972985] <> site "stackoverflow" <> key "Lhg6xe5d5BvNK*C0S8jijA((") >>= \xs →
-    (xs ^.. traverse . to f) ^! to mconcat . act print
- where
-  f ∷ SE Badge → (Sum Int, Sum Int, Sum Int)
-  f (SE x) = (\l → set l (x ^. field "award_count" . to Sum) mempty) $
-    case x ^. field "rank" ∷ String of
-      "bronze" → _1
-      "silver" → _2
-      "gold"   → _3
-      _        → error "badge rank isn't bronze/silver/gold"
+main = ask >>= print . foldMap count
+
+
+ask ∷ IO [SE Badge]
+ask = askSE $ badgesOnUsers [972985] <> site "stackoverflow" <> key "Lhg6xe5d5BvNK*C0S8jijA(("
+
+
+count ∷ SE Badge → (Sum Int, Sum Int, Sum Int)
+count (SE x) = (\l → set l (x ^. field "award_count" . to Sum) mempty) $
+  case x ^. field "rank" ∷ String of
+    "bronze" → _1
+    "silver" → _2
+    "gold"   → _3
+    _        → error "badge rank isn't bronze/silver/gold"
