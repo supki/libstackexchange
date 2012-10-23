0c127aedbf9e32d26d6ac6ed31a625377f962b50
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-10-23 18:33:20 +0400

    Fix badge counter.

--- examples/badges-watcher.hs
+++ examples/badges-watcher.hs
@@ -1,10 +1,10 @@
 #!/usr/bin/env runhaskell
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE UnicodeSyntax #-}
 
-import Data.List (intercalate)
-import Data.Monoid ((<>))
+import Data.Monoid
 
 import Control.Lens
 
@@ -14,7 +14,12 @@ import Network.StackExchange hiding (filter)
 main ∷ IO ()
 main =
   askSE (badgesOnUsers [972985] <> site "stackoverflow" <> key "Lhg6xe5d5BvNK*C0S8jijA((") >>= \xs →
-    (xs ^.. traverse . to unSE . field "rank") ^! act (putStrLn . intercalate " " . sequence (map f ["gold", "silver", "bronze"]))
+    (xs ^.. traverse . to f) ^! to mconcat . act print
  where
-  f ∷ String → [String] → String
-  f t = show . length . filter (== t)
+  f ∷ SE Badge → (Sum Int, Sum Int, Sum Int)
+  f (SE x) = (\l → set l (x ^. field "award_count" . to Sum) mempty) $
+    case x ^. field "rank" ∷ String of
+      "bronze" → _1
+      "silver" → _2
+      "gold"   → _3
+      _        → error "badge rank isn't bronze/silver/gold"
